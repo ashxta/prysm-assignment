88caf394adea0195f1a00d8b3e90d31e1aebc626
Ayush Singhal <ayushsinghal304@gmail.com>
2025-09-15 21:04:27 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/ban-ts-comment": ["error", {
+        "ts-ignore": "allow-with-description",
+        "ts-expect-error": "allow-with-description"
+      }]
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
